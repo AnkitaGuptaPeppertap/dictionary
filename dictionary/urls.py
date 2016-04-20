5c1ba4aa920a7309c1bcf129d8d292f82c3db3b2
Ankita Gupta <ankita.gupta@peppertap.com>
2016-04-20 14:55:57 +0530

    Serving static_root in urls.py

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from apps.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/$', search_words, name="search_words"),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 ]
